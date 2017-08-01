e741e63be508c8f3b752d1c395d95ec827549ee6
RefatIbnAnwar <refatibnanwar@gmail.com>
2017-08-01 16:09:01 +0600

    googleMap

---
+++ GoogleMap/APICall.swift
@@ -0,0 +1,21 @@
+//
+//  APICall.swift
+//  GoogleMap
+//
+//  Created by MobioApp on 5/23/17.
+//  Copyright © 2017 MobioApp. All rights reserved.
+//
+
+import UIKit
+typealias jsonStandard = [String:Any]
+class APICall: NSObject {
+
+    func getDataFromJson(url: String, parameter: String, completion: @escaping (_ success: AnyObject) -> Void) {
+        
+        //@escaping...If a closure is passed as an argument to a function and it is invoked after the function returns, the closure is @escaping.
+       
+    }
+    
+
+    
+}
